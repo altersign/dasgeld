e7f2dd7b9e503cb5380a1281852b5e1a52a57171
Yaroslav Polyakov <contact@altersign.com>
2017-11-26 01:42:07 +0100

    config: add extra db connect configuration: unicode and force foreign key restrictions

--- app/lib/Dasgeld/Config.pm
+++ app/lib/Dasgeld/Config.pm
@@ -7,6 +7,11 @@ my %_config = (
     database => {
         driver => 'SQLite',
         name   => 'db/dasgeld.db',
+        SQLite => {
+            sqlite_unicode => 1,
+            RaiseError     => 1,
+            on_connect_do  => "PRAGMA foreign_keys = ON",
+        }
     }
 );
 
@@ -19,8 +24,13 @@ has get => (
 sub connect_info {
     my ($self) = @_;
 
-    my $db_config = $self->get->{database};
-    return "dbi:$db_config->{driver}:$db_config->{name}";
+    my $db_config    = $self->get->{database};
+    my %connect_info = (
+        dsn => "dbi:$db_config->{driver}:$db_config->{name}",
+        $db_config->{ $db_config->{driver} }->%*
+    );
+
+    return \%connect_info;
 }
 
 __PACKAGE__->meta->make_immutable;
