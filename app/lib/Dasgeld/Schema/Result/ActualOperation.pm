7c5228c6a6646b84fa3158fff08903c8ed0b9584
Yaroslav Polyakov <contact@altersign.com>
2019-02-05 21:45:09 +0100

    Add relations to ActualOperation view

--- app/lib/Dasgeld/Schema/Result/ActualOperation.pm
+++ app/lib/Dasgeld/Schema/Result/ActualOperation.pm
@@ -1,4 +1,5 @@
 use utf8;
+
 package Dasgeld::Schema::Result::ActualOperation;
 
 # Created by DBIx::Class::Schema::Loader
@@ -27,7 +28,7 @@ use base 'DBIx::Class::Core';
 
 =cut
 
-__PACKAGE__->load_components("InflateColumn::DateTime", "TimeStamp");
+__PACKAGE__->load_components( "InflateColumn::DateTime", "TimeStamp" );
 __PACKAGE__->table_class("DBIx::Class::ResultSource::View");
 
 =head1 TABLE: C<actual_operation>
@@ -96,34 +97,80 @@ __PACKAGE__->table("actual_operation");
 =cut
 
 __PACKAGE__->add_columns(
-  "operation_id",
-  { data_type => "integer", is_nullable => 1 },
-  "account_id",
-  { data_type => "integer", is_nullable => 1 },
-  "amount",
-  { data_type => "integer", is_nullable => 1 },
-  "description",
-  { data_type => "text", is_nullable => 1 },
-  "tags",
-  { data_type => "text", is_nullable => 1 },
-  "performed",
-  { data_type => "timestamp", is_nullable => 1 },
-  "created",
-  { data_type => "timestamp", is_nullable => 1 },
-  "parent_operation_id",
-  { data_type => "integer", is_nullable => 1 },
-  "is_actual",
-  { data_type => "boolean", is_nullable => 1 },
-  "revision",
-  { data_type => "integer", is_nullable => 1 },
-  "editor_id",
-  { data_type => "integer", is_nullable => 1 },
+    "operation_id",
+    { data_type => "integer", is_nullable => 1 },
+    "account_id",
+    { data_type => "integer", is_nullable => 1 },
+    "amount",
+    { data_type => "integer", is_nullable => 1 },
+    "description",
+    { data_type => "text", is_nullable => 1 },
+    "tags",
+    { data_type => "text", is_nullable => 1 },
+    "performed",
+    { data_type => "timestamp", is_nullable => 1 },
+    "created",
+    { data_type => "timestamp", is_nullable => 1 },
+    "parent_operation_id",
+    { data_type => "integer", is_nullable => 1 },
+    "is_actual",
+    { data_type => "boolean", is_nullable => 1 },
+    "revision",
+    { data_type => "integer", is_nullable => 1 },
+    "editor_id",
+    { data_type => "integer", is_nullable => 1 },
 );
-
 
 # Created by DBIx::Class::Schema::Loader v0.07046 @ 2017-05-07 23:44:33
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:R8eNw/911fAwWGdq+oIq4A
 
+=head1 RELATIONS
 
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
+=head2 account
+
+Type: belongs_to
+
+Related object: L<Dasgeld::Schema::Result::Account>
+
+=cut
+
+__PACKAGE__->belongs_to(
+    "account",
+    "Dasgeld::Schema::Result::Account",
+    { account_id => "account_id" },
+    {   is_deferrable => 0, on_delete => "NO ACTION", on_update => "NO ACTION"
+    },
+);
+
+=head2 editor
+
+Type: belongs_to
+
+Related object: L<Dasgeld::Schema::Result::Person>
+
+=cut
+
+__PACKAGE__->belongs_to(
+    "editor",
+    "Dasgeld::Schema::Result::Person",
+    { person_id => "editor_id" },
+    {   is_deferrable => 0, on_delete => "NO ACTION", on_update => "NO ACTION"
+    },
+);
+
+=head2 operation_tags
+
+Type: has_many
+
+Related object: L<Dasgeld::Schema::Result::OperationTag>
+
+=cut
+
+__PACKAGE__->has_many(
+    "operation_tags",
+    "Dasgeld::Schema::Result::OperationTag",
+    { "foreign.operation_id" => "self.operation_id" },
+    { cascade_copy           => 0, cascade_delete => 0 },
+);
+
 1;
