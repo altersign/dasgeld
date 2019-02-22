5962d32a049aea6d93383823073ed8454962cdbc
Yaroslav Polyakov <contact@altersign.com>
2019-02-23 00:20:38 +0100

    Result::Tag: add many-to-many relation to operations

--- app/lib/Dasgeld/Schema/Result/Tag.pm
+++ app/lib/Dasgeld/Schema/Result/Tag.pm
@@ -90,6 +90,7 @@ __PACKAGE__->has_many(
 # Created by DBIx::Class::Schema::Loader v0.07046 @ 2017-03-16 01:48:58
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:efD5pJFEcH8UgKK+ESkLiA
 
+__PACKAGE__->many_to_many('operations' => 'operation_tags', 'operation');
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
